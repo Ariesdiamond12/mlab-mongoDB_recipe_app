0896621e3982b5bd10216dc39b36d6201dd6a91f
Precious <92827167+Ariesdiamond12@users.noreply.github.com>
2024-11-26 22:18:09 +0200

    Updated the code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,23 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+const protect = async (req, res, next) => {
+    let token;
+    console.log(req.headers.authorization)
+    if(req.authorization && req.headers.authorization.startWith("Bearer")){
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select("-password");
+            // next is a function in express that passes control to the next middleware function
+            next()
+        } catch (error) {
+            res.status(401).json({error: error})
+        }
+    } else{
+        res.status(401).json({error: "No token, authorization denied"})
+    }
+}
+
+export default protect;
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import recipeController from '../controllers/recipeController.js';
 import userController from '../controllers/userController.js';
+import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router(); //the router object will help sorting our API endpoints
 // We defining our user routes
@@ -8,10 +9,27 @@ router.post('/user', userController.registerUser)
 router.post('/user/login', userController.loginUser)
 
 //We are defining our recipe routes
-router.post('/recipe', recipeController.createRecipe); //this is the endpoint for creating a new recipe
-router.get('/recipe', recipeController.getRecipe)
-router.get('/recipe/:id', recipeController.getRecipeById)
-router.put('/recipe:id', recipeController.updateRecipe)
-router.delete('/recipe/:id',recipeController.deleteRecipe)
+router.post('/recipe', protect, recipeController.createRecipe); //this is the endpoint for creating a new recipe
+router.get('/recipe', protect, recipeController.getRecipe)
+router.get('/recipe/:id', protect, recipeController.getRecipeById)
+router.put('/recipe:id',protect, recipeController.updateRecipe)
+router.delete('/recipe/:id', protect, recipeController.deleteRecipe)
 
 export default router; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
